fd9cac64cf489608ce085656cfe871a78be9c0a2
xzy <zee_yu@163.com>
2020-08-17 18:04:32 +0800

    update

--- app/src/main/java/com/xzy/weather/MainActivity.java
+++ app/src/main/java/com/xzy/weather/MainActivity.java
@@ -1,14 +1,31 @@
 package com.xzy.weather;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.xzy.weather.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        return true;
     }
 }
