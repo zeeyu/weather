6e89c1e92cd64d8fe9c49e1511296461d37fad85
xzy <zee_yu@163.com>
2020-08-31 20:14:41 +0800

    更新添加城市界面

--- app/src/main/java/com/xzy/weather/city/CityAddActivity.java
+++ app/src/main/java/com/xzy/weather/city/CityAddActivity.java
@@ -3,7 +3,7 @@ package com.xzy.weather.city;
 import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -12,11 +12,24 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.xzy.weather.R;
 import com.xzy.weather.base.BaseActivity;
+import com.xzy.weather.bean.MyLocationBean;
+import com.xzy.weather.util.DataStoreUtil;
+import com.xzy.weather.util.TimeUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import interfaces.heweather.com.interfacesmodule.bean.base.Lang;
+import interfaces.heweather.com.interfacesmodule.bean.base.Range;
+import interfaces.heweather.com.interfacesmodule.bean.geo.GeoBean;
+import interfaces.heweather.com.interfacesmodule.view.HeConfig;
+import interfaces.heweather.com.interfacesmodule.view.HeWeather;
 
 public class CityAddActivity extends BaseActivity {
+
+    private static final String TAG = "CityAddActivity";
 
     @BindView(R.id.ed_city_add_search)
     SearchEditText edSearch;
@@ -30,6 +43,9 @@ public class CityAddActivity extends BaseActivity {
     LinearLayout.LayoutParams params1 = (LinearLayout.LayoutParams) edSearch.getLayoutParams();
     LinearLayout.LayoutParams params2 = (LinearLayout.LayoutParams) btnCancel.getLayoutParams();
 
+    List<MyLocationBean> topCityList = new ArrayList<>();
+    MyLocationBean local;
+
     public static int BUTTON_WIDTH = 100;
 
     @Override
@@ -42,7 +58,13 @@ public class CityAddActivity extends BaseActivity {
 
     @Override
     protected void initData() {
-
+//        String time = DataStoreUtil.getLocationInfoUpdateTime(this, location.getId());
+//        if(TimeUtil.getIntervalHour(TimeUtil.getHourNow(), TimeUtil.getTimeHour(time), TimeUtil.getDateNow(), TimeUtil.getTimeDate(time)) < 0.1f){
+//
+//            updateList();
+//        }
+        //TODO
+        getHeWeatherData();
         initView();
     }
 
@@ -55,22 +77,56 @@ public class CityAddActivity extends BaseActivity {
         toolbar.setNavigationOnClickListener(v -> finish());
         toolbar.setTitle(getString(R.string.label_city_add));
 
-        edSearch.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(hasFocus){
-                    params1.width = linearLayout.getWidth() - BUTTON_WIDTH;
-                    params2.width = BUTTON_WIDTH;
-                    edSearch.setLayoutParams(params1);
-                    btnCancel.setLayoutParams(params2);
-                }
+        edSearch.setOnFocusChangeListener((v, hasFocus) -> {
+            if(hasFocus){
+                params1.width = linearLayout.getWidth() - BUTTON_WIDTH;
+                params2.width = BUTTON_WIDTH;
+                edSearch.setLayoutParams(params1);
+                btnCancel.setLayoutParams(params2);
             }
         });
 
-        btnCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btnCancel.setOnClickListener(v -> {
+            params1.width = linearLayout.getWidth();
+            params2.width = 0;
+            edSearch.setLayoutParams(params1);
+            edSearch.setText("");
+            btnCancel.setLayoutParams(params2);
+        });
 
+
+    }
+
+    protected void updateList(){
+        //TODO
+    }
+
+    public void getHeWeatherData(){
+
+        HeConfig.init(getString(R.string.HE_ID), getString(R.string.HE_KEY));
+        HeConfig.switchToDevService();
+
+        HeWeather.getGeoTopCity(this, 20, Range.CN, Lang.ZH_HANS, new HeWeather.OnResultGeoListener() {
+            @Override
+            public void onError(Throwable throwable) {
+                Log.d(TAG, "onError : getGeoTopCity");
+            }
+
+            @Override
+            public void onSuccess(GeoBean geoBean) {
+                List<GeoBean.LocationBean> locationBeanList = geoBean.getLocationBean();
+                for(GeoBean.LocationBean locationBean : locationBeanList){
+                    topCityList.add(new MyLocationBean(
+                            locationBean.getId(),
+                            locationBean.getName(),
+                            locationBean.getCountry(),
+                            locationBean.getAdm2(),
+                            locationBean.getAdm1(),
+                            locationBean.getType(),
+                            false,
+                            false));
+                }
+                updateList();
             }
         });
     }
